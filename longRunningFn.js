daa5147449af965b8462db5baf924590c8438180
Mahboob <mahboob.eid@googlemail.com>
2016-11-21 16:36:16 +0530

    using !== and === instead of != and ==

--- longRunningFn.js
+++ longRunningFn.js
@@ -1,6 +1,6 @@
 self.onmessage = function(e){
     var name = "Hello " + e.data.name.firstName;
-    if(e.data.name.lastName != 'na')
+    if(e.data.name.lastName !== 'na')
         name = name + " !! Is your family name '" + e.data.name.lastName + "'?";
 
     self.postMessage(name);
