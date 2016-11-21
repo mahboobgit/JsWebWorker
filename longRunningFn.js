f27e372f2390b07917af28bf6dd1375c3a1af87e
Mahboob <mahboob.eid@googlemail.com>
2016-11-21 16:27:16 +0530

    Javascript Worker Script Demo. Will run directly on IE but with chrome and Firefox we will have to host the app in IIS or equivalent server to run

---
+++ longRunningFn.js
@@ -0,0 +1,8 @@
+self.onmessage = function(e){
+    var name = "Hello " + e.data.name.firstName;
+    if(e.data.name.lastName != 'na')
+        name = name + " !! Is your family name '" + e.data.name.lastName + "'?";
+
+    self.postMessage(name);
+    
+};
